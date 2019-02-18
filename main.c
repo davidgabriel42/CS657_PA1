a7820c82bc2a1e44d518eb1419bad4a9fcd7919c
David Gabriel <contact@david-gabriel.com>
2019-02-18 01:03:57 -0800

    first commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+typedef char sql_line[144];
+typedef sql_line line_buffer [144];
+
+std::string input_buffer;
+
+int main()
+{
+	int linecount;
+	int wordcount;
+
+	cin >> input_buffer;
+
+
+
+
+}
