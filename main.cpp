e2a03d9d510b9c70f5792bfca4f9aeb03674cd7c
David Gabriel <contact@david-gabriel.com>
2019-02-20 21:50:38 -0800

    CREATE DATABASE  trying error handling

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-
 #include <iostream>
 #include <sys/stat.h>
+#include <regex>
+#include <string>
 
 using namespace std;
 char scan_buffer;
@@ -13,32 +14,35 @@ Takes a string which is the directory name. Will create directory in "./" or ret
 */
 
 int mkdir(std::string dir_name)
-{
-	std::string dir_path_complete_temp = "./" + dir_name;
-	char* dir_path_complete = &dir_path_complete_temp[0u];
+{       
 
-	const int dir_err = mkdir(dir_path_complete, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-	if (-1 == dir_err)
-	{
-	    printf("Error creating directory!n");
-	    exit(1);
-	}
+        struct stat info;
+
+        if( stat( pathname, &info ) != 0 )
+                printf( "cannot access %s\n", pathname );
+        else if( info.st_mode & S_IFDIR )  // S_ISDIR() doesn't exist on my windows 
+                //printf( "%s is a directory\n", pathname );
+		printf("!Failed to create database");
+                printf(dir_name);
+                printf("because it already exists.\r\n")
+        else
+                printf( "%s is no directory\n", pathname );
+        	std::string dir_path_complete_temp = "./" + dir_name;
+        	char* dir_path_complete = &dir_path_complete_temp[0u];
+                int dir_err;
+                dir_err = mkdir(dir_path_complete, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+
 }
 
 /*
 rename- depreciates unused databases
-
 */
 
 int rmdir(std::string dir_name)
 {
-
 	std::string rm_cmd = "rm -r -f ./" + dir_name;
-	system("rm -r -f ./dir1");
-
-
+	system(&rm_cmd[0U]);
 }
-
 
 /*
 mktable- fuction to create a table within a database. The table is stored as a text file.
@@ -58,19 +62,47 @@ int main()
 	int word_count[144];
 	word_count[0] = 0;
 
-//	cin>>input_buffer;
-
 	while(exit_switch)
 	{
 		//interactive shell sdtio
 		std::string line;
 		std::cout << "dave-sql> ";
 		getline (std::cin, line);
-//		std::cout << "Hello, " << line << "!\n";
+
+		std::string result;
+/*
+		std::regex create_database ("CREATE DATABASE");
+  		std::smatch match;
+		if (std::regex_search(line, match, create_database) && match.size() > 1) 
+		{
+			result = match.str(1);
+			mkdir("dir1");
+			printf("make database"); 
+		}
+*/
+		if(std::regex_match (line, std::regex("(CREATE DATABASE)(.*)" )))
+		{
+			//std::cout << "string literal matched\n";
+			std::string database_name = line.erase(0, 16);
+			std::string::size_type semicolon = line.find(";");
+                        database_name = database_name.erase(semicolon, 1);
+			mkdir(database_name);
+			
+                        std::cout << database_name + "\n";
+                       
+		}
+/*
+		std::string::size_type i = t.find(s);
+
+		if (i != std::string::npos)
+		   t.erase(i, s.length());
+*/
+
 		if(line == "exit"){return 0;}
-		if(line == "mkdir"){mkdir("dir1");}
 		if(line == "rmdir"){rmdir("dir1");}
 
+		
+		/*
 		int input_size = line.size();
 
 		for(int i = 0; i < input_size; i++)
@@ -82,20 +114,16 @@ int main()
 					line_count++;
 					word_count[line_count] = 0;
 				break;
-				
+
 				case ' ':
 					//line_length[line_count]++;
 					word_count[line_count]++;
 				break;
-				
+
 				default:
 				break;
 				}
+				*/
 		line.clear();
-
-		}
-		printf("line count:%i \n", line_count);
-		printf("word_count:%i \n", word_count[0]);
-
 	}
 }
